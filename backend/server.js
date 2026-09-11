0d8f1658ea745be3cb8d617f2d23e36ffabd0a40
avanishgupta07 <avanishguptaaza@gmail.com>
2026-09-12 01:37:27 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "https://neon-lebkuchen-323087.netlify.app",
     "https://data-visulization-blackcoffer.netlify.app"
   ]
 }));
